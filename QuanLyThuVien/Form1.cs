73bd039af703d44cce201d3964db8daa69c3e3fd
Sothienthu2006 <enmu2712@gmail.com>
2026-07-04 20:52:52 +0700

    ...
    ...

--- QuanLyThuVien/Form1.cs
+++ QuanLyThuVien/Form1.cs
@@ -16,5 +16,52 @@ namespace QuanLyThuVien
         {
             InitializeComponent();
         }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "Admin" && txtPassword.Text == "123")
+            {
+                labelError.Visible = false;
+                Dashboard ds = new Dashboard();
+                this.Hide();
+                ds.Show();          
+            }
+            else
+            {
+                labelError.Visible = true;
+                txtPassword.Clear();
+            }
+        }
+
+        private void guna2PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2HtmlLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
